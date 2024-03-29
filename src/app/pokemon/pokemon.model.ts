09b47416a0578e464d10eeb1a62bd7699a3429e6
reaganhsmith <reagansmith@byui.edu>
2024-03-29 17:20:35 -0600

    getting bootstrap to work

---
+++ src/app/pokemon/pokemon.model.ts
@@ -0,0 +1,14 @@
+export class Contact {
+    constructor (
+                 public id: string,
+                 public name: string,
+                 public type: string,
+                 public weakness: string,
+                 public number: string,
+                 public color: string,
+                 public evolution: string,
+                 public category: string,
+                 public img: string
+    ) { }
+
+  }
