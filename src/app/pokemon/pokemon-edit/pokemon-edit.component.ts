db9b7005a01a5f90593958b5d0e25bb267e2005f
reaganhsmith <reagansmith@byui.edu>
2024-03-29 16:03:43 -0600

    Adding Angular and creating header/pokemon c

---
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-edit.component.html',
+  styleUrl: './pokemon-edit.component.css'
+})
+export class PokemonEditComponent {
+
+}
