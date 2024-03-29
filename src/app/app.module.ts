45b966ed639065d6958b0106149a4b97839634ab
reaganhsmith <reagansmith@byui.edu>
2024-03-29 16:45:32 -0600

    adding pokemon files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { PokemonDetailsComponent } from './pokemon/pokemon-details/pokemon-details.component';
+import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+import { PokemonItemComponent } from './pokemon/pokemon-list/pokemon-item/pokemon-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PokemonComponent,
+    PokemonDetailsComponent,
+    PokemonEditComponent,
+    PokemonListComponent,
+    PokemonItemComponent
   ],
   imports: [
     BrowserModule,
